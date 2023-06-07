f28842c11402887a777655cba2dbda64ada47a29
NQQT <nguyen.dreamax@gmail.com>
2023-06-07 22:52:14 +1000

    Backup data

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const React = require('react');
+import ReactDOM from 'react-dom/client';
+
+const { StoryGPTMain } = require('app');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(StoryGPTMain));
